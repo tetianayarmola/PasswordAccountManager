96fe2ff49ccfb98441b95c35c9ce44148ce7db23
tetianayarmola <yarmola.acc1@gmail.com>
2025-02-05 16:53:20 +0000

    ssome changes to the password generator form

---
+++ References.md
@@ -0,0 +1,11 @@
+05/02 - Sprint meting - Basic styling and routing.concentrate on the Block Storsge to write and save to file before the next meeting.
+Source: https://vanillarouter.js.org/routing/create-a-router
+
+https://www.geeksforgeeks.org/how-to-create-a-horizontal-navigation-bar-in-html-and-css/
+
+https://www.w3schools.com/css/css_navbar_horizontal.asp
+https://www.w3schools.com/CSS/css_selectors.asp
+
+how to write content to JSON: https://www.w3schools.io/file/json-javascript/
+
+links opening attributes: https://www.w3schools.com/tags/att_a_target.asp
--- main.js
+++ main.js
@@ -1,22 +1,39 @@
+const passwordForm = document.querySelector('#password_form');
+var input_error = false;
+
+function getFormCredentials(){
+    const name = document.querySelector('#name').value;
+    const username = document.querySelector('#username').value;
+    const url = document.querySelector('#url_input').value;
+    const password_value = document.querySelector('#new_password_by_user').value;
+}
+
+
+
 function generate(){
+    
     //get user input reference
-    const length = document.getElementById("length").value;
-    const numbers = document.getElementById("numbers").checked;
-    const uppercase = document.getElementById("uppercase").checked;
-    const lowercase = document.getElementById("lowercase").checked;
-    const symbols = document.getElementById("symbols").checked;
-    const no_duplicates = document.getElementById("no_duplicates").checked;
+    const length = document.querySelector('#length').value;
+    const numbers = document.querySelector('#numbers').checked;
+    const uppercase = document.querySelector('#uppercase').checked;
+    const lowercase = document.querySelector('#lowercase').checked;
+    const symbols = document.querySelector('#symbols').checked;
+    const no_duplicates = document.querySelector('#no_duplicates').checked;
     //generate a random password
     const password = handlePasswordGenerator(parseInt(length), numbers, lowercase, uppercase, symbols, no_duplicates);
     
     //display the pasword
-    document.getElementById('output').textContent = password;
+    document.querySelector('#output').textContent = password;
+    if (!input_error){
+        document.querySelector('#new_password_by_user').value = password;
+    }
+    
 }
 
 
 //hide the password visibility
 function PasswordVisibility(){
-    var item = document.getElementById("output");
+    var item = document.querySelector('#output');
     if (item.type === "password"){
         item.type = "text";
     }
@@ -29,7 +46,7 @@ function PasswordVisibility(){
 //creting a function to generate a random password
 
 function handlePasswordGenerator(length, numbers, lowercase, uppercase, symbols, no_duplicates){
-    
+    input_error = false;
     //create a variable to hold characters set
     let charSet = '';
     //set max characters length variable
@@ -38,6 +55,7 @@ function handlePasswordGenerator(length, numbers, lowercase, uppercase, symbols,
     //handle negative length input
     while (length < 1)
     {
+        input_error = true;
         return `Password length must be a positive number. Please, try again.`;
     }
 
@@ -64,7 +82,8 @@ function handlePasswordGenerator(length, numbers, lowercase, uppercase, symbols,
 
     //check if the length input by user does not exceed maxLength, so it won't generate duplicates
     if (no_duplicates && length > maxLength){
-       return `Password length must be less than ${maxLength} characters. Please, try again.`;
+        input_error = true;
+        return `Password length must be less than ${maxLength} characters. Please, try again.`;
     }
 
 
